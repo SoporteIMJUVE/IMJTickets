22d9c1ea6c7ed84d951c368598fe74d96315d5db
LEVI697 <joshualevirv@gmail.com>
2026-03-25 14:38:47 -0600

    Actualización loading

--- resources/views/components/loading.blade.php
+++ resources/views/components/loading.blade.php
@@ -5,11 +5,10 @@
 
 <div wire:loading.delay.class="opacity-100 visible" 
      wire:target="{{ $target }}" 
-     class="opacity-0 invisible transition-all duration-500 fixed inset-0 bg-black/30 backdrop-blur-sm z-[100] pointer-events-auto"></div>
-
-<div wire:loading.delay.class="opacity-100 visible" 
-     wire:target="{{ $target }}" 
-     class="opacity-0 invisible transition-all duration-500 fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-[110] bg-white border border-gray-100 shadow-2xl rounded-2xl p-7 flex flex-col items-center gap-3">
-    <span class="loading loading-dots loading-lg text-imjuve"></span>
-    <span class="font-semibold text-black animate-pulse">{{ $message }}</span>
+     class="opacity-0 invisible transition-all duration-500 fixed inset-[-1px] w-screen h-screen bg-black/50 backdrop-blur-md z-[9999] pointer-events-auto flex items-center justify-center overflow-hidden">
+    
+    <div class="bg-white border-2 border-gray-200 shadow-2xl rounded-2xl p-8 flex flex-col items-center gap-3">
+        <span class="loading loading-dots loading-lg text-imjuve"></span>
+        <span class="font-bold text-black animate-pulse">{{ $message }}</span>
+    </div>
 </div>
