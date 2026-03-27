8ef035476aa1119d965d1b70fb4a31c71fb75723
LEVI697 <joshualevirv@gmail.com>
2026-03-27 16:36:13 -0600

    Nueva versión

--- resources/views/livewire/navbar.blade.php
+++ resources/views/livewire/navbar.blade.php
@@ -5,7 +5,7 @@
 
         {{-- Versión --}}
         <span class="absolute left-5 translate-y-3 text-xs text-gray-300">
-            v1.1.1
+            v1.1.2
         </span>
         
         {{-- Logo --}}
