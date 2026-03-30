eae3833f6cd5af833e70ee25555a48eb123e7cee
LEVI697 <joshualevirv@gmail.com>
2026-03-30 12:20:27 -0600

    Ajustes finales gestiónn tickets para contraseña y cuentas

--- app/Livewire/Forms/TicketForm.php
+++ app/Livewire/Forms/TicketForm.php
@@ -21,15 +21,16 @@ class TicketForm extends EmailForm
                 // Closure para personalizar las validaciones y sus mensajes
                 function (string $attribute, mixed $value, \Closure $fail) {
                     // Contraseñas
-                    if (preg_match('/(cambi|olvid|re?esta|reset|perdi|nueva|solicit|reconoc|se?p[ae]|acuerd|recuerd|nuev|crea).*(contra)/i', $value)) {
+                    $regexContra = '/(cambi|olvid|re?esta|reset|perdi|nueva|solicit|reconoc|se?p[ae]|acuerd|recuerd|nuev|crea|proble).*(contra|clav)/i';
+                    if (preg_match($regexContra, $value) || preg_match('/.*(contra|clav).*/i', $value)) {
                         $fail('Los trámites de contraseñas deben solicitarse a través de correo electrónico');
                     }
 
                     // Acceso y Gestión de Cuentas
-                    $regexAcceso = '/(no.*(pued|permite|deja)|sin.*acce|bloque).*(ingres|entr|acce).*(correo|cuenta)/i';
-                    $regexGestion = '/(crea|alta|nuev|baja|elimina).*(correo|cuenta|usuari)/i';
+                    $regexAcceso = '/(sin acce|(no.*(pued|permite|deja|bloque).*(ingres|entr|acce).*)).*(correo|cuenta|usuari)/i';
+                    $regexGestion = '/(crea|alta|nuev|baja|elimina|cambi).*(correo|cuenta|usuari)/i';
 
-                    if (preg_match($regexAcceso, $value) || preg_match($regexGestion, $value)) {
+                    if (preg_match($regexAcceso, $value) || preg_match($regexGestion, $value) || preg_match('/.*(correo|cuenta|usuari).*/i', $value)) {
                         $fail('Los trámites de cuentas deben solicitarse mediante oficio');
                     }
                 },
