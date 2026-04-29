d465740e217bf7dd3b7a5a012f0c9b655793b3cc
julio3125 <142358993+julio3125@users.noreply.github.com>
2026-04-29 14:48:41 -0600

    Agregue el puerto en get_conn

--- app.py
+++ app.py
@@ -12,11 +12,11 @@ load_dotenv()
 def get_conn():
     return psycopg2.connect(
         host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT", "5432"),
         database=os.getenv("DB_NAME"),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASS")
     )
-
 # ════════════════════════════════════════
 # FUNCIONES PDF
 # ════════════════════════════════════════
